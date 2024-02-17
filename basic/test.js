a27091306fc89e8cc805ba8bde1810b4c0406cc3
hemantakumarsahoo <140637062+hemantakumarsahoo@users.noreply.github.com>
2024-02-17 17:30:05 +0000

    day1

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,10 @@
 console.log("hello");
+const accountId=12345;
+let custName="hemanta";
+var accountPassward="123456";//prefer not to use var because  of issue in block scope and functional scope
+accountCity="jaipur";
+let accounState
+
+//accountId="3"//notallowed because constant value can not be changed
+console.log(accountId)
+console.table([accountId,accountPassward,accountCity,custName,accounState]);
