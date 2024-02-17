b04a80e2225e8cf2b6e91f828074a0c0ca0bf99a
hemantakumarsahoo <140637062+hemantakumarsahoo@users.noreply.github.com>
2024-02-17 17:49:32 +0000

    data types

---
+++ basic/datatype.js
@@ -0,0 +1,13 @@
+//"use strict;//treat"//treat all js code as newer version
+//alert(3+3)//we are using nodejs ,not in browser
+let name="hemanta";//string-1
+let age=20;//number-2
+let isLoggedIn=false//boolean-3
+let x=99999999999999999999999;//bigint-4
+//null=>standalone value-5
+let fName//undefined value is not defined-6
+//symbol=> unique//to find uniquness//to defined individual componet-7
+//all 7 are premitive data type
+//object
+console.log(typeof(null))//object
+console.log(typeof(undefined))//undefined
