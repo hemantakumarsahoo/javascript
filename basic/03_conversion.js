b244b1fcf60fa85d08c76f9957a3386d1c4b3ddc
hemantakumarsahoo <140637062+hemantakumarsahoo@users.noreply.github.com>
2024-02-17 20:17:20 +0000

    increment decrement

--- basic/03_conversion.js
+++ basic/03_conversion.js
@@ -55,9 +55,60 @@
 // console.log(booleanLogIn);
 
 //numer to string conversion
-let someNumber=30;
+// let someNumber=30;
 
-let number=String(someNumber);
-console.log(typeof(number))//string
-console.log(number);
+// let number=String(someNumber);
+// console.log(typeof(number))//string
+// console.log(number);
 //////////////////////////////////////////////////
+// let value =3;
+// let negValue=-value
+// console.log(negValue);
+
+//operation in js
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="hemanta";
+// let str2="kumar";
+// let str3= str1+str2;
+// console.log(str3);
+
+// console.log(1+2);//3
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log(1+2+"2");//32
+// console.log("1"+2+2);//122
+// console.log(1+"2"+2);//122
+// console.log(1+2+3+"2"+2);//622
+// console.log(1+2+"2"+2);//322
+
+console.log(+true);//1
+// console.log(true+);//eror
+console.log(+"");//0
+
+// let num1,num2,num3;
+// num1=num2=num3=2+2;
+// console.log(num1)
+
+ //prefix and suffix (increment and decrement operator)
+ let gameCounter=100;
+ console.log(gameCounter);
+ game=++gameCounter;//PREFIX
+ console.log(game);
+ console.log(gameCounter);
+
+
+//  let gameCounter=100;
+//  console.log(gameCounter);
+//  game=gameCounter++;
+//  console.log(game);
+//  console.log(gameCounter);
+ 
+
+
+
