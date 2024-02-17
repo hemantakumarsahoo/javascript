6cd879dece431db851540e9a528573dcc506bd94
hemantakumarsahoo <140637062+hemantakumarsahoo@users.noreply.github.com>
2024-02-17 19:14:17 +0000

    conversion

---
+++ basic/03_conversion.js
@@ -0,0 +1,63 @@
+// let score=33;
+
+// console.log(typeof score);//number
+// console.log(typeof (score));//number
+// let score="33";
+
+// console.log(typeof score);//string
+// console.log(typeof (score));//string
+// let score="33";
+
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueInNumber=Number(score)//type conversion string to number
+// console.log(typeof valueInNumber);//number
+// let score="33aa";
+
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueInNumber=Number(score)//type conversion string to number
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//Nan
+// let score=null;
+
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueInNumber=Number(score)//type conversion string to number
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//0
+// let score=undefined;
+
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueInNumber=Number(score)//type conversion string to number
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//nan
+
+//boolean conversion
+
+// let logIn=1;
+
+// let booleanLogIn=Boolean(logIn);
+// console.log(typeof(booleanLogIn))
+// console.log(booleanLogIn);
+
+// let logIn='hemanta';
+
+// let booleanLogIn=Boolean(logIn);
+// console.log(typeof(booleanLogIn))
+// console.log(booleanLogIn);
+
+// let logIn="";
+
+// let booleanLogIn=Boolean(logIn);
+// console.log(typeof(booleanLogIn))
+// console.log(booleanLogIn);
+
+//numer to string conversion
+let someNumber=30;
+
+let number=String(someNumber);
+console.log(typeof(number))//string
+console.log(number);
+//////////////////////////////////////////////////
