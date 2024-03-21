1b70e131e81d82767e3a4836241c450bf74204b3
Hemanta Kumar Sahoo <140637062+hemantakumarsahoo@users.noreply.github.com>
2024-03-21 18:07:23 +0000

    FOR EACH LOOP

--- basic3/07_foreach.js
+++ basic3/07_foreach.js
@@ -38,7 +38,29 @@ const myCoding=[
 
 ]
 myCoding.forEach((element) => {
-    console.log(element.languageName)
+    //console.log(element.languageName)
     //console.log(element.languageFileName)
     
 });
+
+//filter map reduce  video-30
+//  const value=coding.forEach((item)=>{//for each loop not return any value
+//     console.log(item);
+//     return item
+
+// })
+// console.log(value);
+
+// const myNum=[1,2,3,4,5,6,7,8,9,10]
+// const newNum=myNum.filter((num)=>num>4)
+// console.log(newNum)
+const myNum=[1,2,3,4,5,6,7,8,9,10]
+const newNum=myNum.filter((num)=>{
+    return num>4
+}//if i open the scope  use return key word
+)
+console.log(newNum)
+
+
+cont newNum=[]
+myN
